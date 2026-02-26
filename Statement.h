81487cc8886b78c0172ee8c287dcfc41f4a429ac
EthanTheProfessionalIdiot <157541505+EthanTheProfessionalIdiot@users.noreply.github.com>
2026-02-26 16:59:58 -0600

    Update Statement.h

--- Statement.h
+++ Statement.h
@@ -5,21 +5,26 @@
 #include "Account.h"
 #include "Date.h"
 
+class Account;  // forward declaration
+class Date;     // forward declaration
+
+
 class Statement {
 private:
     std::string statementId;
     std::string content;
-    Account destination;
-    Date dateSent;
+    Account* destination;   // pointer
+    Date* dateSent;         // pointer
 
 public:
     Statement();   // ADD THIS
     void generate(int string_content);
     std::string getContent();
     void setContent(std::string content);
-    Account getDestination();
-    void setDestination(Account destination);
+    Account* getDestination();
+    void setDestination(Account* destination);
 };
 
 #endif
+
 
