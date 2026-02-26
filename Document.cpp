e6e2eeac8b90bbc02618098cc5ee3eafd3c62b46
EthanTheProfessionalIdiot <157541505+EthanTheProfessionalIdiot@users.noreply.github.com>
2026-02-26 13:21:24 -0600

    Update Document.cpp

--- Document.cpp
+++ Document.cpp
@@ -1,35 +1,68 @@
 #include "Document.h"
+#include <string>
+#include <sstream>
 
-Document::Document() {
-	// TODO - implement Document::Document
-	throw "Not yet implemented";
+// If Account is defined elsewhere, make sure this is included
+// #include "Account.h"
+
+Document::Document()
+{
+    this->pageCount = 0;
+    this->content = "";
+    this->documentId = "";
+    this->documentIs = "";
 }
 
-void Document::Generate() {
-	// TODO - implement Document::Generate
-	throw "Not yet implemented";
+void Document::Generate()
+{
+    // Simple generation logic for now
+    // Page count based on content length (example rule: 500 chars per page)
+
+    if (content.length() == 0)
+    {
+        pageCount = 0;
+        return;
+    }
+
+    pageCount = (content.length() / 500) + 1;
+
+    // Optional: auto-generate documentId
+    std::stringstream ss;
+    ss << "DOC-" << content.length() << "-" << pageCount;
+    documentId = ss.str();
 }
 
-string Document::getContent() {
-	return this->content;
+string Document::getContent()
+{
+    return this->content;
 }
 
-void Document::setContent(string content) {
-	this->content = content;
+void Document::setContent(string content)
+{
+    this->content = content;
 }
 
-Account Document::getRecipient() {
-	return this->recipient;
+Account Document::getRecipient()
+{
+    return this->recipient;
 }
 
-void Document::setRecipient(Account recipient) {
-	this->recipient = recipient;
+void Document::setRecipient(Account recipient)
+{
+    this->recipient = recipient;
 }
 
-int Document::getPageCount() {
-	return this->pageCount;
+int Document::getPageCount()
+{
+    return this->pageCount;
+}
+
+void Document::setPageCount(int pageCount)
+{
+    this->pageCount = pageCount;
 }
 
 void Document::setPageCount(int pageCount) {
 	this->pageCount = pageCount;
 }
+
