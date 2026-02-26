08a3f111367cae483133f98de0d4cdfac09d10a4
EthanTheProfessionalIdiot <157541505+EthanTheProfessionalIdiot@users.noreply.github.com>
2026-02-26 13:21:44 -0600

    Update Document.h

--- Document.h
+++ Document.h
@@ -1,31 +1,33 @@
 #ifndef DOCUMENT_H
 #define DOCUMENT_H
 
+#include <string>
+using std::string;
+
+#include "Account.h"  // make sure this exists
+
 class Document {
 
 private:
-	int pageCount;
-	string content;
-	Account recipient;
-	string documentId;
-	string documentIs;
+    int pageCount;
+    string content;
+    Account recipient;
+    string documentId;
+    string documentIs;  // consider removing if typo
 
 public:
-	Document();
+    Document();
 
-	void Generate();
+    void Generate();
 
-	string getContent();
+    string getContent();
+    void setContent(string content);
 
-	void setContent(string content);
+    Account getRecipient();
+    void setRecipient(Account recipient);
 
-	Account getRecipient();
-
-	void setRecipient(Account recipient);
-
-	int getPageCount();
-
-	void setPageCount(int pageCount);
+    int getPageCount();
+    void setPageCount(int pageCount);
 };
 
 #endif
